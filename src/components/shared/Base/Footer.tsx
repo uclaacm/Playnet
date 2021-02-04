a1a74e990ffafc4cd0c3c4c9358ce7938a37cf2e
Arjun Raghavan <arjun.raghavan.2k@gmail.com>
2021-02-03 22:31:50 -0500

    Added footer

--- src/components/shared/Base/Footer.tsx
+++ src/components/shared/Base/Footer.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
+import '../../styles/Footer.scss';
+
+import CLLogo from '../../../assets/logos/creative_labs.svg';
+import TLALogo from '../../../assets/logos/teach_la.svg';
+
 function Footer(): JSX.Element {
   return (
-    <div><h2>Footer</h2></div>
+    <div id={'footer'}>
+      <h2>Made with ❤️ by <img src={CLLogo} /> + <img src={TLALogo} /> acm.teachLA!</h2>
+      <p id={'disclaimer'}>
+        Not sponsored by or affiliated with Youtube or its subsidiaries.
+      </p>
+    </div>
   );
 }
 
