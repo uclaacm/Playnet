32750bf4e78ba09a696956bd4f84eb4ad2760464
Bryan Pan <bryanpan342@gmail.com>
2021-02-10 12:25:29 -0800

    add comments

--- src/utils/cipher.ts
+++ src/utils/cipher.ts
@@ -1,16 +1,27 @@
-const HASHING_FACTOR = 26;
-
+/**
+ * Return a numerical representation of a hash string
+ *
+ * @param hashString a string to convert into a hashed number
+ */
 function hashNumber(hashString: string): number {
   let hash = 0;
   hashString.split('').map((char) => {
     hash = ((hash << 5) - hash) + char.charCodeAt(0);
     hash |= 0; // Convert to a number
-  })
+  });
   return hash;
 }
 
+/**
+ * Return a Caesar Cipher encoding of an input word
+ *
+ * @param hash a hashed string, representing the shift value
+ * @param input an input string to be encoded
+ */
 export function scramble(hash: string, input: string): string {
-  const shift = hashNumber(hash) % HASHING_FACTOR;
+  // Obtain a 32 bit representation of the hash
+  const shift = hashNumber(hash);
+
   return input.split('').reduce((ret, char) => {
     // Obtain the ascii value of the character
     const ascii = char.charCodeAt(0);
