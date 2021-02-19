dd53dd957c333188fbb097589174070de66fa96f
Regina Wang <rrreggginnna@gmail.com>
2021-02-19 13:01:18 -0800

    mlem

---
+++ src/components/CarouselItem.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+interface CarouselItemProps {
+  children?: JSX.Element;
+  showNext?: boolean;
+  showPrevious?: boolean;
+}
+
+function CarouselItem(props: CarouselItemProps) : JSX.Element {
+  return <div></div>;
+}
+
