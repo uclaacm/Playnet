77e4f0b32e7597128f53d46c41940241f8708705
Bryan Pan <bryanpan342@gmail.com>
2021-02-22 18:40:45 -0800

    feat: implement activities home screen

--- src/components/Activities.tsx
+++ src/components/Activities.tsx
@@ -11,6 +11,40 @@ import Activity2 from './Activities/Activity2';
 import Activity3 from './Activities/Activity3';
 import Base from './shared/Base';
 import { HeaderSections } from './shared/PlaynetConstants';
+import ActivityCard from './shared/ActivityCard';
+
+import './styles/Activities.scss';
+
+/**
+ * The following is a mapping between an activity and its descirption
+ */
+const ACTIVITES: {[key: string]: string} = {
+  'lost-in-translation': 'When you use the search bar, how does YouTube know what you’re looking for?',
+  'sending-videos': 'Videos hold a lot of information! How can pages load quickly?',
+  'mind-reading': 'Have you ever wondered how YouTube knows what you want to watch next?',
+}
+
+interface ActivityHomeProps {
+  url: string;
+}
+
+function ActivityHome(props: ActivityHomeProps): JSX.Element {
+  const { url } = props;
+  return (
+    <div id={'activity-home'}>
+      <h2>Activities</h2>
+      <nav id={'activity-card-container'}>
+        {Object.keys(ACTIVITES).map((path, i) =>
+          <Link to={`${url}/${path}`} key={`activites-card-${i}`}>
+            <ActivityCard
+              id={path}
+              description={ACTIVITES[`${path}`]}/>
+          </Link>
+        )}
+      </nav>
+    </div>
+  );
+}
 
 function Activities(): JSX.Element {
   const { url, path } = useRouteMatch();
@@ -20,13 +54,13 @@ function Activities(): JSX.Element {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path={`${path}/1`}>
+          <Route exact path={`${path}/lost-in-translation`}>
             <Activity1 />
           </Route>
-          <Route path={`${path}/2`}>
+          <Route exact path={`${path}/sending-videos`}>
             <Activity2 />
           </Route>
-          <Route path={`${path}/3`}>
+          <Route exact path={`${path}/mind-reading`}>
             <Activity3 />
           </Route>
           <Route path="/">
@@ -34,26 +68,6 @@ function Activities(): JSX.Element {
           </Route>
         </Switch>
       </Base>
-    </div>
-  );
-}
-
-function ActivityHome(props: { url: string }): JSX.Element {
-  return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to={`${props.url}/1`}>Activity 1</Link>
-          </li>
-          <li>
-            <Link to={`${props.url}/2`}>Activity 2</Link>
-          </li>
-          <li>
-            <Link to={`${props.url}/3`}>Activity 3</Link>
-          </li>
-        </ul>
-      </nav>
     </div>
   );
 }
