7e93d20e25a5fb6e3cb23ecbdd0609b2a8adc4e3
Bryan Pan <bryanpan342@gmail.com>
2021-02-22 19:16:15 -0800

    remove unneccessary div

--- src/components/Feedback.tsx
+++ src/components/Feedback.tsx
@@ -6,21 +6,19 @@ import './styles/Feedback.scss';
 
 function Feedback(): JSX.Element {
   return (
-    <div>
-      <Base section={HeaderSections.FEEDBACK}>
-        <div id={'feedback-container'}>
-          <h2>Feedback</h2>
-          <p>
-            So that’s a very simple overview as to how Youtube works!
-            <br/>
-            If you liked the problems in that space, maybe you’d like computer science :)
-            <br/>
-            <br/>
-            Got any feedback? We’d love to improve!
-          </p>
-        </div>
-      </Base>
-    </div>
+    <Base section={HeaderSections.FEEDBACK}>
+      <div id={'feedback-container'}>
+        <h2>Feedback</h2>
+        <p>
+          So that’s a very simple overview as to how Youtube works!
+          <br/>
+          If you liked the problems in that space, maybe you’d like computer science :)
+          <br/>
+          <br/>
+          Got any feedback? We’d love to improve!
+        </p>
+      </div>
+    </Base>
   );
 }
 
