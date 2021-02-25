a468f4b133e775be88e812094c09d4e4141e6a39
Regina Wang <rrreggginnna@gmail.com>
2021-02-25 14:43:17 -0800

    refactor: add favicon and make footer thinner (#33)
    - add favicon
    - move carousel to shared
    - make footer thinner (as requested by designers a while back)
    - update logo
    closes #32

--- src/components/shared/Base/Footer.tsx
+++ src/components/shared/Base/Footer.tsx
@@ -8,7 +8,7 @@ import TLALogo from '../../../assets/logos/teach_la.svg';
 function Footer(): JSX.Element {
   return (
     <div id={'footer'}>
-      <h2>Made with ❤️ by <a href={'https://creativelabsucla.com'} target={'_blank'} rel={'noopener noreferrer'}><img id={'cl-logo'} src={CLLogo} /></a> + <a href={'https://teachla.uclaacm.com'} target={'_blank'} rel={'noopener noreferrer'}> <img id={'tla-logo'} src={TLALogo} /></a></h2>
+      <h3>Made with ❤️ by <a href={'https://creativelabsucla.com'} target={'_blank'} rel={'noopener noreferrer'}><img id={'cl-logo'} src={CLLogo} /></a> + <a href={'https://teachla.uclaacm.com'} target={'_blank'} rel={'noopener noreferrer'}> <img id={'tla-logo'} src={TLALogo} /></a></h3>
       <p id={'disclaimer'}>
         Not sponsored by or affiliated with Youtube or its subsidiaries.
       </p>
