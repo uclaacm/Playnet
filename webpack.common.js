8cd733210ed2e7e5ef1125b16cb563f31a03a621
Regina Wang <rrreggginnna@gmail.com>
2021-02-25 14:02:51 -0800

    svg instead of ico because better quality

--- webpack.common.js
+++ webpack.common.js
@@ -31,7 +31,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
-      favicon: './src/assets/logos/favicon.svg',
+      favicon: './public/favicon.svg',
       filename: './index.html',
     }),
     new CleanWebpackPlugin(),
