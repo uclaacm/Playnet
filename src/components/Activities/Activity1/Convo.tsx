d49bd33a9906a248abce37003a3ad975a1692a38
Darren Zhang <darrenzhang22@gmail.com>
2021-02-26 13:46:43 -0800

    Attempt to remove CRLF endings

--- src/components/Activities/Activity1/Convo.tsx
+++ src/components/Activities/Activity1/Convo.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import Anime, {anime} from 'react-anime';
 
 import '../../styles/Activity1.scss';
 import { TextBubbleStyles } from '../../shared/PlaynetConstants';
@@ -23,7 +22,7 @@ function Convo(props: ConvoProps): JSX.Element {  //to replay convo, set i to 0
         </div>;
       }
 
-      return <div className='fade'>
+      return <div className='fade' style={{animationDuration: 2/3*props.delayTime/1000+'s'}}>
         {(props.phrases[i].indexOf('.svg') === -1) ?
           <TextBubble key={i} text={props.phrases[i]} textBubbleStyle={props.textBubbleStyle}/> :
           <TextBubble key={i} contentSvg={props.phrases[i]} textBubbleStyle={props.textBubbleStyle}/>
@@ -45,12 +44,6 @@ function Convo(props: ConvoProps): JSX.Element {  //to replay convo, set i to 0
   return (
     <div>
       {props.phrases.map(displaySpeechBubbles)}
-      {/* <Anime delay={anime.stagger(100)}
-         scale={[.1, .9]}>
-        <div className="blue">Hello</div>
-        <div className="green"> World</div>
-        <div className="red">!!!</div>
-      </Anime>  */}
     </div>
   );
 }
