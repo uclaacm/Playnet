e093cf8c1e06d6fd287b35512405855c7d645f78
Darren Zhang <darrenzhang22@gmail.com>
2021-02-28 01:04:01 -0800

    Removed map and replaced with a function that returns a single div

--- src/components/Activities/Activity1/Convo.tsx
+++ src/components/Activities/Activity1/Convo.tsx
@@ -14,22 +14,19 @@ interface ConvoProps {
 function Convo(props: ConvoProps): JSX.Element {  //to replay convo, set i to 0
   const [i, setI] = useState(0);
 
-  const displaySpeechBubbles = (text: string) => {
-    if (props.phrases[i] === text) {
-      if (i === 0 && props.offset !== undefined && props.phrases[i] === ''){
-        return <div key={'blank'} className='hidden'>
-          <TextBubble key={i} text={''} textBubbleStyle={props.textBubbleStyle}/>
-        </div>;
-      }
-
-      return <div className='fade' style={{animationDuration: 2/3*props.delayTime/1000+'s'}}>
-        {(props.phrases[i].indexOf('.svg') === -1) ?
-          <TextBubble key={i} text={props.phrases[i]} textBubbleStyle={props.textBubbleStyle}/> :
-          <TextBubble key={i} contentSvg={props.phrases[i]} textBubbleStyle={props.textBubbleStyle}/>
-        }
+  const DisplaySpeechBubbles = () => {
+    if (i === 0 && props.offset !== undefined && props.phrases[i] === ''){
+      return <div key={'blank'} className='hidden'>
+        <TextBubble key={i} text={''} textBubbleStyle={props.textBubbleStyle}/>
       </div>;
     }
-    return <div key={text}></div>;
+
+    return <div className='fade' style={{animationDuration: 2/3*props.delayTime/1000+'s'}}>
+      {(props.phrases[i].indexOf('.svg') === -1) ?
+        <TextBubble key={i} text={props.phrases[i]} textBubbleStyle={props.textBubbleStyle}/> :
+        <TextBubble key={i} contentSvg={props.phrases[i]} textBubbleStyle={props.textBubbleStyle}/>
+      }
+    </div>;
   };
 
   useEffect(() => {
@@ -43,7 +40,7 @@ function Convo(props: ConvoProps): JSX.Element {  //to replay convo, set i to 0
 
   return (
     <div>
-      {props.phrases.map(displaySpeechBubbles)}
+      <DisplaySpeechBubbles/>
     </div>
   );
 }
