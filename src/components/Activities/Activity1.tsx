b5ecdba5851fa9622636ed20d69e3364f43d5e1b
Darren Zhang <56695604+darren2hang@users.noreply.github.com>
2021-03-03 19:48:23 -0800

    feat: made first 3 screens of activity 1 (#38)
    - Made TextBubble component that displays cartton speech bubbles
    - Made Convo component that can display multiple text bubbles in a row
    - fixed EOL to LF
    Closes: #11

--- src/components/Activities/Activity1.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Activity1(): JSX.Element {
-  return (
-    <div><h1>Activity 1</h1></div>
-  );
-}
-
-export default Activity1;
---
+++ src/components/Activities/Activity1/TextBubble.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { TextBubbleStyles } from '../../shared/PlaynetConstants';
+
+import '../../styles/TextBubble.scss';
+
+interface TextBubbleProps {
+  text?: string;
+  contentSvg?: string;   //if broken change back to any
+  textBubbleStyle: TextBubbleStyles,
+}
+function TextBubble(props: TextBubbleProps): JSX.Element {
+  return (
+    <div id={props.textBubbleStyle} className='text-bubble'>
+      {(props.contentSvg !== undefined) ? <img src={props.contentSvg} className='centered'/>: ''}
+      {(props.text !== undefined) ? <span className='centered'>{props.text}</span> : ''}
+    </div>
+  );
+}
+
+export default TextBubble;
