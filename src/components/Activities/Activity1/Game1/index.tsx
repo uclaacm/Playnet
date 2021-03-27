f493508e2061cd7db38cf4d5255a3cb29ca07330
Trevor <tr89on@gmail.com>
2021-03-26 17:14:26 -0700

    use new Alien component

--- src/components/Activities/Activity1/Game1/index.tsx
+++ src/components/Activities/Activity1/Game1/index.tsx
@@ -4,19 +4,19 @@ import '../../../styles/Game.scss';
 
 import useSound from 'use-sound';
 
+import Alien, { ALIEN_STATE } from '../../../shared/Alien';
 import Star from '../../../../assets/activity1/game1/star.svg';
 import CorrectSFX from '../../../../assets/activity1/game2/correct.mp3';
 import IncorrectSFX from '../../../../assets/activity1/game2/oh_no_1.mp3';
 
-import Pair1A from '../../../../assets/activity1/game2/pair1a.svg';
-import Pair1B from '../../../../assets/activity1/game2/pair1b.svg';
-import Pair2A from '../../../../assets/activity1/game2/pair2a.svg';
+import Pair1A from '../../../../assets/activity1/game1/twoapples.svg';
+import Pair1B from '../../../../assets/activity1/apple.svg';
+import Pair2A from '../../../../assets/activity1/lemon.svg';
 import Pair2B from '../../../../assets/activity1/game2/pair2b.svg';
 import Pair3A from '../../../../assets/activity1/game2/pair3a.svg';
 import Pair3B from '../../../../assets/activity1/game2/pair3b.svg';
 import Pair4A from '../../../../assets/activity1/game2/pair4a.svg';
 import Pair4B from '../../../../assets/activity1/game2/pair4b.svg';
-import AlienSvg from '../../../../assets/alien.svg';
 
 import { scramble } from '../../../../utils';
 import { CarouselContext } from '../../../shared/Carousel';
@@ -112,7 +112,7 @@ function Game1(): JSX.Element {
           flexDirection: 'row',
           justifyContent: 'center',
           alignItems: 'center'}}>
-          <img src={AlienSvg} alt="friendly alien"/>
+          {/* <Alien alienState={ALIEN_STATE.BASE} /> */}
           {starCounter()}
         </div>
         <br/>
@@ -125,12 +125,20 @@ function Game1(): JSX.Element {
 
   const displayGame = () => {
     return showSuccess ? displaySuccess() :
-      (<div>
-        <h3> Try to guess what image the alien wants.</h3>
+      <div className={'game-1-cards'}>
+        <SlideBox handleClick={()=>handleClick(0)} imgSrc={Pair1A} />
+        <SlideBox handleClick={()=>handleClick(1)} imgSrc={Pair1B} />
+      </div>;
+  };
+
+  return (
+    <div id={'game-wrapper'}>
+      <h3> Try to guess what image the alien wants.</h3>
+      <div>
         <div id={'game-content'}>
           <div className={'gamebox'}>
             <TextBubble textBubbleStyle={TextBubbleStyles.EXTRA_LARGE} text={displayText()} />
-            <img src={AlienSvg} alt="friendly alien"/>
+            <Alien alienState={ALIEN_STATE.BASE} />
             <div style={{
               width: '75%',
               display: 'flex',
@@ -142,18 +150,11 @@ function Game1(): JSX.Element {
             </div>
             Happiness
           </div>
-          <div className={'game-1-cards'}>
-            <SlideBox handleClick={()=>handleClick(0)} imgSrc={Pair1A} />
-            <SlideBox handleClick={()=>handleClick(1)} imgSrc={Pair1B} />
-          </div>
+          {displayGame()}
         </div>
         {starCounter()}
-      </div>);
-  };
-
-  return (
-    <div id={'game-wrapper'}>
-      {displayGame()}
+      </div>
+      {/* {displayGame()} */}
     </div>
   );
 }
