a9a4808f83eb90ab33da143a105b0e025695949f
Regina Wang <rrreggginnna@gmail.com>
2021-03-30 12:44:12 -0700

    grab commit

---
+++ src/components/Home/FinalSlide.tsx
@@ -0,0 +1,54 @@
+import anime, {AnimeTimelineInstance} from 'animejs';
+import React, { useEffect, useRef } from 'react';
+
+function FinalSlide(props): JSX.Element {
+  const timeline = useRef<AnimeTimelineInstance | null>(null);
+  useEffect(() => { // why useEffect??
+    timeline.current = anime.timeline({
+      autoplay: false,
+      delay: 250,
+      easing: 'easeInOutExpo',
+    });
+    
+    timeline.current?.add({
+      targets: '#final-intro-background',
+      opacity: [1, 0],
+      duration: 1000,
+    })
+    .add({
+      targets: ['#rocket', '#video-pulley', '#video'],
+      opacity: [0,1],
+      duration: 1000,
+    })
+    .add({
+      targets: ['#server', '#computer'],
+      translateX: 1000,
+    })
+    .add({
+      targets: ['#rocket', '#video-pulley', '#video'],
+      translateX: -1000,
+      duration: 1000,
+      easing: 'easeInExpo',
+    })
+    .add({
+      targets: ['#rocket', '#video-pulley'],
+      translateX: -1000,
+      duration: 1000,
+      easing: 'none',
+    })
+    .add({
+      targets: ['#computer', '#video'],
+      scale: [1, 2],
+      duration: 1000,
+    });
+  })
+  return <div id={'intro-container'}>
+    <div id={'final-intro-background'}/>
+    <div id={'server'}/>
+    <div id={'video-pulley'}/>
+    <div id={'video'} className={props.video}/>
+    <div id={'rocket'}/>
+    <div id={'computer'}/>
+  </div>
+}
+export default FinalSlide;
