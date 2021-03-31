b3939c04d038a40fd758b4b203c34abfc82ca99b
Regina Wang <rrreggginnna@gmail.com>
2021-03-31 15:51:02 -0700

    finish final slide animation

--- src/components/Home/FinalSlide.tsx
+++ src/components/Home/FinalSlide.tsx
@@ -1,66 +1,83 @@
-import anime, {AnimeTimelineInstance} from 'animejs';
+import anime, { AnimeTimelineInstance } from 'animejs';
 import React, { useEffect, useRef } from 'react';
 import { VideoChoices } from '../shared/PlaynetConstants';
+import '../styles/FinalSlide.scss';
 
 interface FinalSlideProps {
-<<<<<<< HEAD
   chosenVideo: VideoChoices,
 }
 
 function FinalSlide(props: FinalSlideProps): JSX.Element {
-=======
-  chosenVideo: string,
-}
-
-function FinalSlide(props): JSX.Element {
->>>>>>> 2beed87 (save)
   const timeline = useRef<AnimeTimelineInstance | null>(null);
-  useEffect(() => { // why useEffect??
+  const timeout = useRef<NodeJS.Timeout | null>(null);
+  useEffect(() => {
     timeline.current = anime.timeline({
       autoplay: false,
       delay: 250,
-      easing: 'easeInOutExpo',
+      easing: 'easeInOutSine',
     });
-    
+
     timeline.current?.add({
       targets: '#final-intro-background',
       opacity: [1, 0],
       duration: 1000,
     })
     .add({
+      targets: ['#server'],
+      translateY: 80,
+      duration: 500,
+    }, '-=50')
+    .add({
       targets: ['#rocket', '#video-pulley', '#video'],
       opacity: [0,1],
       duration: 1000,
-    })
+    }, '-=50')
     .add({
       targets: ['#server', '#computer'],
-      translateX: 1000,
+      translateX: 685,
+      easing: 'easeInSine',
+      duration: 1000,
     })
     .add({
-      targets: ['#rocket', '#video-pulley', '#video'],
-      translateX: -1000,
+      targets: ['#rocket', '#video'],
+      left: -265,
       duration: 1000,
-      easing: 'easeInExpo',
-    })
+      easing: 'easeInSine',
+    }, '-=100')
     .add({
-      targets: ['#rocket', '#video-pulley'],
-      translateX: -1000,
+      targets: ['#rocket'],
+      left: -1000,
       duration: 1000,
-      easing: 'none',
-    })
+      easing: 'linear',
+    }, '-=50')
     .add({
       targets: ['#computer', '#video'],
-      scale: [1, 2],
+      scale: [1, 3],
       duration: 1000,
-    });
-  })
-  return <div id={'intro-container'}>
-    <div id={'final-intro-background'}/>
-    <div id={'server'}/>
-    <div id={'video-pulley'}/>
-    <div id={'video'} className={props.video.replace('_', '-')}/>
-    <div id={'rocket'}/>
-    <div id={'computer'}/>
-  </div>
+    }, '-=50');
+  }, []);
+
+  useEffect(() => {
+    if (timeout.current) {
+      clearTimeout(timeout.current);
+    }
+    timeline.current?.pause();
+    timeline.current?.seek(0);
+    timeout.current = setTimeout(() => {
+      timeline.current?.play();
+    }, 250);
+  }, []);
+
+  return <div id={'final-intro-container'}>
+    <div id={'lottie-mock-container'}>
+      <div id={'final-intro-background'} />
+      <div id={'server'} />
+      <div id={'computer'}/>
+      <div id={'rocket'}>
+        <div id={'video-pulley'}/>
+      </div>
+      <div id={'video'} className={props.chosenVideo.replace('_', '-')}/>
+    </div>
+  </div>;
 }
 export default FinalSlide;
