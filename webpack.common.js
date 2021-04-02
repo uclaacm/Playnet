55f5d71fa18995e25b7006aa7771bd728512df8e
Regina Wang <rrreggginnna@gmail.com>
2021-04-02 14:16:09 -0700

    remove some useless confusing code

--- webpack.common.js
+++ webpack.common.js
@@ -33,7 +33,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
-      // favicon: './public/favicon.svg',
+      favicon: './public/favicon.svg',
       filename: './index.html',
     }),
     new CopyWebpackPlugin({
