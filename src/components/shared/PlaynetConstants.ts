ba4de43b49d128fcc25b9b7bbc90ff891c975c28
Regina Wang <rrreggginnna@gmail.com>
2021-04-03 13:47:35 -0700

    better flow in words

--- src/components/shared/PlaynetConstants.ts
+++ src/components/shared/PlaynetConstants.ts
@@ -2,6 +2,11 @@
 export const FEEDBACK_FORM_URL  =
   'https://docs.google.com/forms/d/e/1FAIpQLSfpCmyRZ7Ilrg263iYYiXLaM8UWX7iK9aSuVl0_u8mguvxgiA/viewform?usp=sf_link';
 
+export const DEFAULT_CONFIGS = {
+  MUTED: false,
+  AUTOPLAY: false,
+};
+
 // Internal Constants
 export enum HeaderSections {
   INTRO = 'intro',
