caa9cbd2f45f0aeb79f77392dec604c5442d176d
Regina Wang <rrreggginnna@gmail.com>
2021-04-03 02:53:19 -0700

    sound-track started mapping and uploaded mp3s

---
+++ src/components/shared/soundtrack.ts
@@ -0,0 +1,9 @@
+import A1_1 from '../../assets/sounds/Activity1/A1-1.mp3';
+ 
+export enum SoundTrack {
+  Activity1_1,
+}
+export const SoundTrackMapping : Record<SoundTrack, any> = {
+  [SoundTrack.Activity1_1] : A1_1,
+  
+}
