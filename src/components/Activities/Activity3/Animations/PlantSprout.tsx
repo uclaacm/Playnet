668a38160b9ba84c622fa9e9aba7a81afee1fbfa
Trevor <tr89on@gmail.com>
2021-04-08 21:01:01 -0700

    initial commit

---
+++ src/components/Activities/Activity3/Animations/PlantSprout.tsx
@@ -0,0 +1,60 @@
+import anime, { AnimeTimelineInstance } from 'animejs';
+import React, { useContext, useEffect, useRef } from 'react';
+import { CarouselContext } from '../../../shared/Carousel';
+import ScalingSlide from '../../../shared/ScalingSlide';
+
+interface ManyEmployeesProps {
+  start: boolean,
+}
+
+function ManyEmployees(props: ManyEmployeesProps): JSX.Element {
+  const { reloadTime } = useContext(CarouselContext);
+  const timeline = useRef<AnimeTimelineInstance | null>(null);
+
+  useEffect(() => {
+    timeline.current = anime.timeline({
+      autoplay: false,
+      easing: 'easeInOutSine',
+    });
+
+    timeline.current?.add({
+      targets: '#left-leaf',
+      opacity: [0, 1],
+      duration: 500,
+    });
+
+    timeline.current?.add({
+      targets: '#plant-sprout',
+      width: [22, 22],
+      height: [0, 197],
+      duration: 1000,
+    });
+
+    timeline.current?.add({
+      targets: '#right-leaf',
+      opacity: [0, 1],
+      duration: 500,
+    });
+  }, []);
+
+  useEffect(() => {
+    if (props.start === true) {
+      const timeout = setTimeout(() => {
+        timeline.current?.play();
+      }, 250);
+      timeline.current?.pause();
+      timeline.current?.seek(0);
+      return () => clearTimeout(timeout);
+    }
+  }, [reloadTime, props.start]);
+
+  return <ScalingSlide widthPx={1102} heightPx={294}>
+    <>
+      <div id={'plant-sprout'} />
+      <div id={'left-leaf'} />
+      <div id={'right-leaf'} />
+      <div id={'bottom-soil'} />
+    </>
+  </ScalingSlide>;
+}
+export default ManyEmployees;
