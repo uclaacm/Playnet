488eeb529cabb8461b0d12d5e9bf3d984bfd12d3
Regina Wang <rrreggginnna@gmail.com>
2021-04-13 23:59:41 -0700

    psuedocoded game wrapper

--- src/components/Activities/Activity3/Game/index.tsx
+++ src/components/Activities/Activity3/Game/index.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import GameSlide from '../../Activity1/Game2/components/GameSlide';
 import { A3_GAME_STATE } from './GameConstants';
 
 export const GameContext = React.createContext({
@@ -9,7 +10,28 @@ export const GameContext = React.createContext({
 
 function Game(): JSX.Element {
   const [state, setState] = useState<A3_GAME_STATE>(A3_GAME_STATE.PriorityExplanation);
-  const [statesToSkip, setStatesToSkip] = useState()
+  const [statesToSkip, setStatesToSkip] = useState(new Set()); // check this out
+
+  useEffect(() => {
+    // onMount, get state from storage
+    // setup statesToSKip from storage
+  }, [])
+
+  const next = (): void => {
+    // stupidly setGameState(next)
+  }
+  useEffect(() => {
+    // add statesToSkip to storage
+  }, [statesToSkip]);
+
+  useEffect(() => {
+    // check that state in statesToSkip
+    // if state not in statesToSkip, go on to next
+    // if state = one time only, add to statesToSkip
+
+    // add state to storage
+  }, [state]);
+
 
   const getGameElement = (): JSX.Element => {
     switch (state) {
@@ -32,7 +54,7 @@ function Game(): JSX.Element {
     }
   }
 
-  <GameContext.Provider value={{gameState: state, setGameState: setState, next: next}}>
+  return <GameContext.Provider value={{ gameState: state, setGameState: setState, next: next }}>
     {
       getGameElement()
     }
