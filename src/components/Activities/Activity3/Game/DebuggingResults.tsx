cd6c71f5ee0e92f35d9670b86ebe73acaa3e3f9d
Regina Wang <rrreggginnna@gmail.com>
2021-04-16 00:33:10 -0700

    init"

---
+++ src/components/Activities/Activity3/Game/DebuggingResults.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function DebuggingResults(): JSX.Element {
+  return <>
+    <div className={"half"}>
+
+    </div>
+    <div className={"half"}>
+
+    </div>
+  </>
+}
