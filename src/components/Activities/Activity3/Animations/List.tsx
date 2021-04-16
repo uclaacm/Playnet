5f6410197bf4cea1ea3ef6902b9757e7670a319d
Regina Wang <rrreggginnna@gmail.com>
2021-04-16 01:14:47 -0700

    animation for list, or slide 10

---
+++ src/components/Activities/Activity3/Animations/List.tsx
@@ -0,0 +1,73 @@
+import anime, { AnimeTimelineInstance } from 'animejs';
+import React, { useContext, useEffect, useRef } from 'react';
+import { CarouselContext } from '../../../shared/Carousel';
+import ScalingSlide from '../../../shared/ScalingSlide';
+
+function List(): JSX.Element {
+  const { reloadTime } = useContext(CarouselContext);
+  const timeline = useRef<AnimeTimelineInstance | null>(null);
+
+  const RECOMMENDATION_PARAMETERS = [
+    'Watch History Video Titles',
+    'Watch History Channels',
+    'Titles',
+    'Search History',
+    'Subscribed Channels',
+    'Country',
+    'Time of Day',
+    'Previous Videos',
+    'Watch Time',
+    'Random Feedback',
+    'User Video Preferred Tags',
+    'User Likes',
+    'Watch Session Length',
+    'Video Clicks',
+    'Preferred Popularity Level',
+    'Video Length',
+    'Video Popularity',
+    'Quality fo Video',
+    'Video Feedback',
+    'Video Number of Views with respect to Time',
+    'Credibility of Video',
+    'Video Content',
+    'Video Title',
+    'Video Tags',
+    'Video Creator',
+    'Video Variety',
+  ];
+  const MAPPED_PARAMETERS = RECOMMENDATION_PARAMETERS.map((element) => <><p/>{element}</>);
+
+  useEffect(() => {
+    timeline.current = anime.timeline({
+      autoplay: false,
+    }).add({
+      targets: '#animated-list',
+      translateY: -2000,
+      duration: 3000,
+      easing: 'easeInQuad',
+    }).add({
+      targets: '#animated-list',
+      translateY: -4600,
+      duration: 3000,
+      easing: 'easeOutQuad',
+    });
+  }, []);
+
+  useEffect(() => {
+    timeline.current?.pause();
+    timeline.current?.seek(0);
+    timeline.current?.play();
+  }, [reloadTime]);
+
+  return <ScalingSlide>
+    <div id={'animated-list-box'}>
+      <div id={'animated-list'}>
+        {MAPPED_PARAMETERS}
+        {MAPPED_PARAMETERS}
+        {MAPPED_PARAMETERS}
+        {MAPPED_PARAMETERS}
+      </div>
+    </div>
+  </ScalingSlide>;
+}
+export default List;
