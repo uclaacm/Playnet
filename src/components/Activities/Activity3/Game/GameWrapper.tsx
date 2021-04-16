07010e684c30221c7ce78c456659efe49fe473eb
Darren <darrenzhang22@gmail.com>
2021-04-15 23:01:57 -0700

    Reset GameWrapper to original

--- src/components/Activities/Activity3/Game/GameWrapper.tsx
+++ src/components/Activities/Activity3/Game/GameWrapper.tsx
@@ -1,34 +1,16 @@
-import React, { useState } from 'react';
-import FeatureSlidebar from './FeaturesSlidebar';
-import { VARIABLES, VARIABLE_POSITIONING } from './GameWrapperConstants';
+import React from 'react';
+import { VARIABLE_POSITIONING } from './GameWrapperConstants';
 import VariableCard from './VariableCard';
 
 function GameWrapper(): JSX.Element {
-  const [i, setI] = useState(0);
-  const slideLength = 2;
-  const slides: JSX.Element[] = [
-    <div id='variables-wrapper'>
-      {
-        VARIABLE_POSITIONING.map(rowElements =>
-          rowElements.map(variable =>
-            <VariableCard id={variable} key={variable} />,
-          ),
-        )
-      }
-    </div>,
-    <FeatureSlidebar choices={[VARIABLES.CREDIBLE, VARIABLES.SUBSCRIBED, VARIABLES.SAME_CONTENT]}/>,
-  ];
-
-  const continueClick = () => {
-    if (i < slideLength) {
-      setI(i+1);
+  return <div id='variables-wrapper'>
+    {
+      VARIABLE_POSITIONING.map(rowElements =>
+        rowElements.map(variable =>
+          <VariableCard id={variable} key={variable} />,
+        ),
+      )
     }
-  }
-  return (
-    <>
-    {slides[i]}
-    <button className='playnet-button' onClick={continueClick}>Continue</button>
-    </>
-  );
+  </div>;
 }
 export default GameWrapper;
