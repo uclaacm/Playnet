1da6f8f1952a6cf43429d0c634fc4134c35d30cd
Trevor <tr89on@gmail.com>
2021-04-17 14:35:52 -0700

    add animation for slides 16-19

--- src/components/Activities/Activity3/index.tsx
+++ src/components/Activities/Activity3/index.tsx
@@ -4,6 +4,7 @@ import '../../styles/Activity3Game.scss';
 
 import Carousel, { CarouselItemComponents } from '../../shared/Carousel';
 import ComputerRecs from './Animations/ComputerRecs';
+import Debugging from './Animations/Debugging';
 import List from './Animations/List';
 import ManyEmployees from './Animations/ManyEmployees';
 import PlantSprout from './Animations/PlantSprout';
@@ -47,6 +48,21 @@ function Activity3(): JSX.Element {
     {
       child: <PlantSprout />,
       bottomText: 'With so many possibilities, how does an idea get brought to life and end up on your screen?',
+      animationTime: 2,
+    },
+    {
+      child: <Debugging />,
+      bottomText: 'We do that by writing code.',
+      animationTime: 2,
+    },
+    {
+      child: <Debugging />,
+      bottomText: 'We also have to debug the code, meaning that we fix errors.',
+      animationTime: 2,
+    },
+    {
+      child: <Debugging />,
+      bottomText: 'We also have to debug the code, meaning that we fix errors.',
       animationTime: 2,
     },
     {
