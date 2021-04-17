b3c7e30009db0d2d42a325645d22e2ae8d22627b
Regina Wang <rrreggginnna@gmail.com>
2021-04-17 10:40:21 -0700

    function frameworks

---
+++ src/components/Activities/Activity3/Game/gameCalculationsUtil.ts
@@ -0,0 +1,25 @@
+/**
+ * 
+ * @returns points between x: [0, 100], y: [0, 100] to graph
+ */
+export function getControlGraphForABTesting(
+  numABTestingDays: number,
+): [number, number][] {
+
+  return [];
+}
+
+/**
+ * 
+ * @returns points between x: [0, 100], y: [0, 100] to graph
+ */
+export function getBetaGraphForABTesting(
+  expectedVariableValues: number[],
+  actualVariableValues: number[],
+  controlGraph: [number, number][],
+  timeAllocations: [number, number, number],
+): [number, number][] {
+
+  return [];
+}
+
