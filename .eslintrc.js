cea8b9b33c063cdb7eda06b66b1f60bfaa07f86b
Regina Wang <rrreggginnna@gmail.com>
2021-05-07 20:21:49 -0700

    add prettier as lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:import/typescript',
     'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
