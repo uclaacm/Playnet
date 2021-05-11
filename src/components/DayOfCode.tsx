0e5ba35f3c57f0961f7b65127a9f67117115bb3e
Darren Zhang <darrenzhang22@gmail.com>
2021-05-10 17:57:10 -0700

    Update Day of Code code word and link

--- src/components/DayOfCode.tsx
+++ src/components/DayOfCode.tsx
@@ -5,19 +5,19 @@ import { HeaderSections } from './shared/PlaynetConstants';
 import './styles/Feedback.scss';
 
 function DayOfCode(): JSX.Element {
-  const link_to_checklist = 'http://links.uclaacm.com/dayofcodechecklist';
+  const link_to_checklist = 'http://links.uclaacm.com/dayofcodepassport';
   return (
     <Base section={HeaderSections.DAY_OF_CODE}>
       <div id={'feedback-container'}>
         <p>
           The code for this event is:
-          <h1><i>C O D</i></h1>
+          <h1><i>C O D I N G</i></h1>
           <button className='playnet-button'>
             <a href={link_to_checklist} target='_blank' rel='noreferrer'>Add it to your passport!</a>
           </button>
           <br />
           <br />
-          <a href={link_to_checklist} style={{textDecoration: 'underline'}} target='_blank' rel='noreferrer'>http://links.uclaacm.com/dayofcodechecklist</a>!
+          <a href={link_to_checklist} style={{textDecoration: 'underline'}} target='_blank' rel='noreferrer'>{link_to_checklist}</a>!
           <br/>
         </p>
       </div>
